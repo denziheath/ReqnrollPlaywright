b6c2f8232e199b564cf8d4484b45079e09fe1242
denziheath <denziheath@yahoo.com>
2026-02-11 10:54:02 -0700

    video eleven completed

---
+++ ReqnrollPlaywright/ReqnrollPlaywright/Drivers/Driver.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReqnrollPlaywright.Drivers
+{
+    internal class Driver
+    {
+    }
+}
--- ReqnrollPlaywright/ReqnrollPlaywright/StepDefinitions/EaAppTestSteps.cs
+++ ReqnrollPlaywright/ReqnrollPlaywright/StepDefinitions/EaAppTestSteps.cs
@@ -11,6 +11,32 @@ namespace ReqnrollPlaywright.StepDefinitions
             _scenarioContext = scenarioContext;
         }
 
+        [Given(@"I enter following login details")]
+
+        public void GivenIEnterFollowingLoginDetails(Table table)
+        {
+            ScenarioContext.StepIsPending();
+        }
+
+
+        [Given("I navigate to the application")]
+        public void GivenINavigateToTheApplication()
+        {
+            // ScenarioContext.StepIsPending is deprecated, this is new -> throw new PendingStepException();
+            ScenarioContext.StepIsPending();
+        }
+
+        [Given("I click login link")]
+        public void GivenIClickLoginLink()
+        {
+            ScenarioContext.StepIsPending();
+        }
+
+        [Then("I see employee lists")]
+        public void ThenISeeEmployeeLists()
+        {
+            ScenarioContext.StepIsPending();
+        }
 
     }
 }
